0139ff6f307dd1b64494a36d03653c7684b4d1a3
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2017-11-13 17:14:13 +0200

    Part1.2 imports

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
@@ -6,3 +6,9 @@ Created on Mon Nov 13 00:22:25 2017
 @author: vzabulskyy
 """
 
+#Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
