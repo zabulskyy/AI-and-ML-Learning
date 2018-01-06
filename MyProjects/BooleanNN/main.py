338973475e60c87779adab416ee7ee615fa7c3c3
zabulskyy <v.zabulskyy@gmail.com>
2018-01-06 12:10:09 +0200

    XOR beta (fail)

--- MyProjects/BooleanNN/main.py
+++ MyProjects/BooleanNN/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+np.random.seed(11)
+
 XOR_data = np.array([
     [1, 1, 0],
     [1, 0, 1],
@@ -7,4 +9,46 @@ XOR_data = np.array([
     [0, 0, 0],
 ])
 
-print(XOR_data[:, 2])
+
+def sigmoid(x):
+    return 1 / (1 + np.e ** (-x))
+
+
+def fit(h, X, y):
+    return np.dot(X.T, (h - y).T)
+
+
+def cost(h, y):
+    h = h[0]
+
+    def c(h, y):
+        if abs(y - h) == 1:
+            return np.infty
+        return -np.log(h) if y == 1 else -np.log(1 - h)
+
+    value = np.zeros(h.size)
+    for i in range(len(y)):
+        value[i] = c(h[i], y[i])
+    return sum(value) / len(value)
+
+
+def hypothesis(w, X):
+    def g(z):
+        return sigmoid(z)
+
+    return g(np.dot(w, X))
+
+
+X = XOR_data[:, :2]
+y = XOR_data[:, 2]
+w = np.random.rand(2, 1)
+
+generations = 100
+a = 1
+
+for i in range(generations):
+    h = hypothesis(w.T, X.T)
+    e = cost(h, y)
+    w -= a / len(w) * fit(h, X, y)
+    print(h)
+
