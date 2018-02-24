13df0ccab549839c54db8f8b2236a5baea0673e3
vzabulskyy <v.zabulskyy@gmail.com>
2018-02-24 16:52:43 +0200

    linear regression fixed bug

--- Coursera/week1/main.py
+++ Coursera/week1/main.py
@@ -27,17 +27,16 @@ def gradient_descent(X, y, theta, a, iters):
 
 def normalize(X):
     X_norm = X[:, 1:]
-    mu = np.mean(X_norm)
-    sigma = np.std(X_norm)
-    print(mu, sigma)
-    X_norm = (X_norm - mu) / sigma
+    mu = np.mean(X_norm, axis=0)
+    sigma = np.std(X_norm, axis=0)
+    sub = np.subtract(X_norm, mu)
+    X_norm = np.divide(sub, sigma)
     X[:, 1:] = X_norm
-    print(X)
     return X
 
 
-if __name__ == "__main__":
-    # y = 2x^2 + 3x + 0
+if 1:
+    # y = 0x^2 + 3x + 10
     X = np.array([
         [1., -2., 4.],
         [1., -1., 1.],
@@ -49,11 +48,16 @@ if __name__ == "__main__":
 
     # X = normalize(X)
 
-    y = np.array([2., -2., 0., 5., 14., 27.])
-    theta = np.array([1., 2., 0.])
-    a, iters = .095, 5000
+    y = np.array([4., 7., 10., 13., 16., 19.])
+    theta = np.array([1., 2., 40])
+    a, iters = 0.05, 1000
     w, h = gradient_descent(X, y, theta, a, iters)
-    print("w = {0}, err = {1}".format(w, h[-1]))
+    print("w = {0}".format(w))
     print(compute_cost(X, y, theta))
 
-    # y = 3x + 10
+
+# x1 = np.arange(12.0).reshape((6, 2))
+# x2 = np.arange(6.0).reshape((6, 1))
+# print(x1, x2)
+# a = np.subtract(x1, x2)
+# print(a)
